dc00a7338885c1ead88ea3ec1b2de95099c9233f
Alberto <alberto0o095bm@gmail.com>
2017-05-05 23:28:09 +0200

    why is this not working wtf

--- Proyects/Acme-Chorbi/src/test/java/services/ManagerServiceTest.java
+++ Proyects/Acme-Chorbi/src/test/java/services/ManagerServiceTest.java
@@ -1,10 +1,5 @@
-package services;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.GregorianCalendar;
+package services;
 
 import javax.validation.ConstraintViolationException;
 
@@ -15,18 +10,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import security.Authority;
-import security.UserAccount;
 import utilities.AbstractTest;
-import domain.Chorbi;
-import domain.Event;
 import domain.Manager;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 	"classpath:spring/junit.xml"
 })
-
 @Transactional
 public class ManagerServiceTest extends AbstractTest {
 
@@ -34,65 +24,60 @@ public class ManagerServiceTest extends AbstractTest {
 	@Autowired
 	private ManagerService	managerService;
 
+
 	// Teoria pagina 107 y 108
 	// Tests ---------------------------------------------------------------
+	//TODO DataIntegrity error a solucionar.
 	@Test
 	public void driverCreation() {
 		final Object testingData[][] = {
 			{		// Creación correcta de un Manager.
-				"correcto", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", null
+				"managerTest1", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", null
 			}, {	// Creación errónea de un Manager: title vacío.
-				"correcto", "correcto", "", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
+				"managerTest2", "correcto", "", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {	// Creación errónea de un Manager: description vacío.
-				"correcto", "correcto", "correcto", "", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321",  ConstraintViolationException.class
+				"managerTest3", "correcto", "correcto", "", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {	// Creación errónea de un Manager: picture que no es url.
 				"", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {	// Creación errónea de un Manager: moment nulo vacío.
-				"correcto", "", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321",  ConstraintViolationException.class
+				"managerTest4", "", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {	// Creación errónea de un Manager: numberSeat negativo.
-				"correcto", "correcto", "correcto", "correcto", "", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
+				"managerTest5", "correcto", "correcto", "correcto", "", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {	// Creación errónea de un Manager: no es un manager el logueado.
-				"correcto", "correcto", "correcto", "correcto", "correcto@bien.com", "", "equisdejajajaxdxdxd", "43321",  ConstraintViolationException.class
+				"managerTest6", "correcto", "correcto", "correcto", "correcto@bien.com", "", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
-				"correcto", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "", "43321", ConstraintViolationException.class
+				"managerTest7", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "", "43321", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
-				"correcto", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "", ConstraintViolationException.class
+				"managerTest8", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
 				"cor", "correcto", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
-				"correcto", "co", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
+				"managerTest9", "co", "correcto", "correcto", "correcto@bien.com", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
-				"correcto", "correcto", "correcto", "correcto", "ajjj", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
+				"managerTest10", "correcto", "correcto", "correcto", "ajjj", "1234", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}, {		// Creación correcta de un Manager.
-				"correcto", "correcto", "correcto", "correcto", "correcto@bien.com", "A", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
+				"managerTest11", "correcto", "correcto", "correcto", "correcto@bien.com", "A", "equisdejajajaxdxdxd", "43321", ConstraintViolationException.class
 			}
 		};
 		for (int i = 0; i < testingData.length; i++)
-			this.templateCreationDelete((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6], (String) testingData[i][7], (Class<?>) testingData[i][8]);
+			this.templateCreationDelete((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (String) testingData[i][6],
+				(String) testingData[i][7], (Class<?>) testingData[i][8]);
 	}
-		
+
 	// Templates ----------------------------------------------------------
-	private void templateCreationDelete(final String username, final String password, final String name, final String surname, final String email, final String phone,
-			final String companyName, final String VATNumber, final Class<?> expected) {
+	private void templateCreationDelete(final String username, final String password, final String name, final String surname, final String email, final String phone, final String companyName, final String VATNumber, final Class<?> expected) {
 		Class<?> caught;
 		caught = null;
 		try {
 			final Manager m = this.managerService.create();
-			final UserAccount user = new UserAccount();
-			final Collection<Authority> as = new ArrayList<Authority>();
-			final Authority a = new Authority();
-			a.setAuthority("MANAGER");
-			as.add(a);
-			user.setAuthorities(as);
-			m.setUserAccount(user);
-			user.setUsername(username);
-			user.setPassword(password);
-			m.setUserAccount(user);
+
+			m.getUserAccount().setUsername(username);
+			m.getUserAccount().setPassword(password);
 			m.setName(name);
 			m.setSurname(surname);
 			m.setEmail(email);
 			m.setPhoneNumber(phone);
-			
+
 			m.setCompanyName(companyName);
 			m.setVATNumber(VATNumber);
 			this.managerService.save(m);
