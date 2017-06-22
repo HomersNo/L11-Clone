d7ae0927fbe72930610a0f1fcfaac375a6a11793
Jultrocor <j.troncosocorrea@gmail.com>
2017-06-22 17:18:28 +0200

    SafeHtml

--- Proyects/Acme-Chorbi/src/main/java/domain/Actor.java
+++ Proyects/Acme-Chorbi/src/main/java/domain/Actor.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
 
 import security.UserAccount;
 
@@ -42,6 +43,7 @@ public abstract class Actor extends DomainEntity {
 
 
 	@NotBlank
+	@SafeHtml
 	public String getName() {
 		return this.name;
 	}
@@ -49,6 +51,7 @@ public abstract class Actor extends DomainEntity {
 		this.name = name;
 	}
 	@NotBlank
+	@SafeHtml
 	public String getSurname() {
 		return this.surname;
 	}
@@ -58,6 +61,7 @@ public abstract class Actor extends DomainEntity {
 
 	@NotBlank
 	@Email
+	@SafeHtml
 	public String getEmail() {
 		return this.email;
 	}
@@ -66,6 +70,7 @@ public abstract class Actor extends DomainEntity {
 	}
 
 	@NotBlank
+	@SafeHtml
 	@Pattern(regexp = "^([+](9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1))?(\\s?\\d){1,14}$")
 	public String getPhoneNumber() {
 		return this.phoneNumber;
